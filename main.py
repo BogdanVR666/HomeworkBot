3c5bcfcf5963b643919ce12bd7f33500e81da0c5
BogdanVR666 <bogdanvr666@gmail.com>
2022-03-04 01:05:48 +0200

    Nobody

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 from urllib3 import disable_warnings
 from bs4 import BeautifulSoup
 from rich import print as rint
-from pprint import pprint
 
 disable_warnings()
 session = requests.Session()
@@ -21,73 +20,74 @@ FormData = {
     "password": "BogdanVR1"
 }
 
-domen = "https://school-5p.e-schools.info"
-css_block = 'a.user_type_1'
-result_link = '/dnevnik/quarter/28553'
+site = "https://school-5p.e-schools.info"
+user_css_block = 'a.user_type_1'
+weekler_link = '/dnevnik/quarter/28553'
 
 
 def update_site(filename):
     assert filename[-5:] == '.html', "type is not html"
-    
+
     with open(filename, "w", encoding="UTF-8") as file:
         file.write(str(pupil_bs))
 
 
 def write_table(filename, days, lessons, homeworks):
-    with open("homeworks.txt", "w", encoding="UTF-8") as result:
+    with open(filename, "w", encoding="UTF-8") as file:
         x = 0
         for i, j in zip(lessons, homeworks):
             if "1." in i and "11." not in i:
-                result.write(f"\n{days[x]}\n")
+                file.write(f"\n{days[x]}\n")
                 x += 1
-            result.write(f"{i} {j}\n") if len(i) > 3 else None
+            file.write(f"{i} {j}\n") if len(i) > 3 else None
 
 
-def create_table(days, lessons, homeworks):
+def create_table(days: list, lessons: list, homeworks: list):
     result = dict()
     score = -1
-    
+
     for i in zip(lessons, homeworks):
         score += 1 if "1." in i[0] and "11." not in i[0] else 0
         try:
-            result[days[score].split()[0]].append((int(i[0][0]), 
-                                                   i[0].split(maxsplit=1)[-1] if len(i[0]) > 3 else '', 
+            result[days[score].split()[0]].append((int(i[0][0]),
+                                                   i[0].split(maxsplit=1)[-1] if len(i[0]) > 3 else '',
                                                    i[1]))
         except KeyError:
-            result[days[score].split()[0]] = [int(days[score][-2:]), 
-                                              (int(i[0][0]), 
+            result[days[score].split()[0]] = [int(days[score][-2:]),
+                                              (int(i[0][0]),
                                                i[0].split(maxsplit=1)[-1] if len(i[0]) > 3 else '', i[1])]
-    
+
     return result
 
 
 class SchoolParser:
-    def __init__(self, domen:str, login:dict):
+    def __init__(self, domen: str, login: dict):
         self.domen = domen
         self.login = login
     
-    def connect(self, link:str, css_block:str, result_link:str):
+    def connect(self, link: str, css_block: str, result_link: str):
         pupil = None
-        
+
         while not pupil:
             answer = session.post(link, data=self.login)
             answer_bs = BeautifulSoup(answer.content, "html.parser")
             pupils = answer_bs.select(css_block)
-            pupil = domen + pupils[0]["href"] + result_link if pupils else None
-        
+            pupil = site + pupils[0]["href"] + result_link if pupils else None
+
         return pupil
 
-school5p = SchoolParser(domen, FormData)
-user_site = school5p.connect('https://school-5p.e-schools.info/login_', css_block, result_link)
+
+school5p = SchoolParser(site, FormData)
+user_site = school5p.connect('https://school-5p.e-schools.info/login_', user_css_block, weekler_link)
 pupil_bs = BeautifulSoup(session.get(user_site).content, "html.parser")
-lessons = [" ".join(str(i.text).split()) for i in pupil_bs.select("td.lesson > span")]
-homeworks = [" ".join(str(i.text).split()) for i in pupil_bs.select("td.ht")]
-days = [i.text for i in pupil_bs.select('th.lesson')]
+week_days = [i.text for i in pupil_bs.select('th.lesson')]
+days_lessons = [" ".join(str(i.text).split()) for i in pupil_bs.select("td.lesson > span")]
+lessons_homeworks = [" ".join(str(i.text).split()) for i in pupil_bs.select("td.ht")]
 
 
 if __name__ == '__main__':
     update_site('parsed.html')
-    write_table('homeworks.txt', days=days, lessons=lessons, homeworks=homeworks)
-    result = create_table(days=days, lessons=lessons, homeworks=homeworks)
-    rint(result)
+    write_table('homeworks.txt', days=week_days, lessons=days_lessons, homeworks=lessons_homeworks)
+    all_of_week = create_table(days=week_days, lessons=days_lessons, homeworks=lessons_homeworks)
+    rint(all_of_week)
     rint('Программа завершена [green]успешно[/green]. Расписание в файле [cyan]homeworks.txt[/cyan]')
