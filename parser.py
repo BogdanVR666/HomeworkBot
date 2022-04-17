53c5255d6d2596e8a223ff01b6ade2861423e064
BogdanVR666 <bogdanvr666@gmail.com>
2022-04-17 23:00:33 +0300

    added JSON

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 import requests
 from urllib3 import disable_warnings
+import json
 from bs4 import BeautifulSoup
 from rich import print as rint
 
@@ -31,34 +32,37 @@ def update_site(filename):
 def write_table(filename, days, lessons, homeworks):
     with open(filename, "w", encoding="UTF-8") as file:
         x = 0
-        for i, j in zip(lessons, homeworks):
-            if "1." in i and "11." not in i:
+        for lesson, homework in zip(lessons, homeworks):
+            if lesson.startswith('1.'):
                 file.write(f"\n{days[x]}\n")
                 x += 1
-            file.write(f"{i} {j}\n") if len(i) > 3 else None
+            if len(lesson) > 3:
+                file.write(f"{lesson} {homework}\n")
 
 
-def create_table(days: list, lessons: list, homeworks: list):
+def create_table(days: list, lessons: list, homeworks: list) -> dict:
     result = dict()
     score = -1
 
-    for i in zip(lessons, homeworks):
-        score += 1 if "1." in i[0] and "11." not in i[0] else 0
+    for line in zip(lessons, homeworks):
+        if line[0].startswith('1.'):
+            score += 1
         try:
-            result[days[score].split()[0]].append((int(i[0][0]),
-                                                   i[0].split(maxsplit=1)[-1] if len(i[0]) > 3 else '',
-                                                   i[1]))
+            result[days[score].split()[0]].append((int(line[0][0]),
+                                                   line[0].split(maxsplit=1)[-1] if len(line[0]) > 3 else '',
+                                                   line[1]))
         except KeyError:
             result[days[score].split()[0]] = [int(days[score][-2:]),
-                                              (int(i[0][0]),
-                                               i[0].split(maxsplit=1)[-1] if len(i[0]) > 3 else '', i[1])]
+                                              (int(line[0][0]),
+                                               line[0].split(maxsplit=1)[-1] if len(line[0]) > 3 else '', line[1])]
 
     return result
 
-responce = 0
-while responce != 200:
+
+server_answer = 0
+while server_answer != 200:
     school5p = session.post("https://school-5p.e-schools.info/login_", data=FormData)
-    responce = school5p.status_code
+    server_answer = school5p.status_code
 
 shodennik = session.get('https://school-5p.e-schools.info/pupil/1056949/dnevnik/quarter/28553')
 print('school5p connected')
@@ -66,13 +70,18 @@ print('school5p connected')
 pupil_bs = BeautifulSoup(shodennik.content, "html.parser")
 print('site parsed')
 
-week_days = [i.text for i in pupil_bs.select('th.lesson')]
-days_lessons = [" ".join(str(i.text).split()) for i in pupil_bs.select("td.lesson > span")]
+week_days = [day.text for day in pupil_bs.select('th.lesson')]
+days_lessons = [" ".join(str(lesson_line.text).split()) for lesson_line in pupil_bs.select("td.lesson > span")]
 lessons_homeworks = [" ".join(str(i.text).split()) for i in pupil_bs.select("td.ht")]
 print('school data initialised')
 
 if __name__ == '__main__':
     update_site('parsed.html')
+    print('parsed.html is rewrited')
     write_table('homeworks.txt', days=week_days, lessons=days_lessons, homeworks=lessons_homeworks)
+    print('homeworks.txt rewrited')
     all_of_week = create_table(days=week_days, lessons=days_lessons, homeworks=lessons_homeworks)
+    with open('homeworks.json', 'w', encoding='UTF-8') as json_file:
+        json.dump(all_of_week, json_file, ensure_ascii=False, indent=4)
+    print('homeworks.json rewrited')
     rint('Программа завершена [green]успешно[/green]. Расписание в файле [cyan]homeworks.txt[/cyan]')
