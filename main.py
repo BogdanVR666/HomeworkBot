acf48298416ef0657e29c16be94b2e8efeccf917
BogdanVR666 <63509709+BogdanVR666@users.noreply.github.com>
2022-04-22 00:17:30 +0300

    Add files via upload
    added /yesteday and /tomorrow commands

--- main.py
+++ main.py
@@ -1,46 +1,54 @@
 import re
+import json
 import asyncio
-from datetime import datetime
-from time import sleep
+from time import strftime
 from aiogram import executor, Bot, Dispatcher, types
-from parser import *
+from rich import print
 
-all_of_week = create_table(days=week_days, lessons=days_lessons, homeworks=lessons_homeworks)
+with open('homeworks.json', 'r', encoding='UTF-8') as json_file:
+    homeworks = json.load(json_file)
 
 template = '''
-Урок: {lesson}
-Ссылка: {link}
-Время: {time}
+Номер урока: {}
+Урок: {}
+Домашнее задание: {}
+Время: {}
+Ссылка: {}
 '''
-
-time_list = list()
-
-for days, lessons in list(all_of_week.items()):
-    for i in range(1, len(lessons)): 
-        if datetime.now().day == lessons[0]:
-            # lessons[i] is (num, lesson, homeworks)
-            regexp_time = re.search(r'\d\d:\d\d (AM|PM)', lessons[i][2]) # search "07:00 AM" at homeworks
-            if regexp_time:
-                regexp_split = regexp_time.group().split() # ['07:00', 'AM']
-                regexp_time = regexp_split[0].split(':') # ['07', '00']
-                if regexp_split[1] == 'PM':
-                    lesson_time = ':'.join((str(int(regexp_time[0]) + 12), regexp_time[1])) # convert 12 time to 24 time
-                else:
-                    lesson_time = regexp_split[0]
-                time_list.append(lesson_time)
-            else: 
-                lesson_time = 'урок без времени'
-            regexp_link = re.search(r'https?://[\S][^>]+', lessons[i][2]) # link search
-            link = regexp_link.group() if regexp_link else None
-            #print(template.format(lesson=lessons[i][1], link=link, time=lesson_time))
-
-print('time of lessons initialised')
 
 async def timer(time):
     while datetime.now() > time:
         asyncio.sleep(30)
 
 bot = Bot(token='5102803513:AAEJRgR_XxoaCQYG81MwTX9zLPxMiGR9vYs')
+dispatcher = Dispatcher(bot)
+
+def return_homeworks(day):
+    result = list()
+    for day in homeworks.values():
+        if day[0] == day:
+            for lesson in day:
+                if isinstance(lesson, list):
+                    result.append(template.format(*lesson))
+    return result
+
+
+@dispatcher.message_handler(commands=['yesteday'])
+async def send_yestesay(message: types.Message):
+    for lesson in return_homeworks(int(strftime('%d')) - 1):
+        await message.answer(lesson)
+
+
+@dispatcher.message_handler(commands=['today'])
+async def send_today(message: types.Message):
+    for lesson in return_homeworks(int(strftime('%d'))):
+        await message.answer(lesson)
+
+
+@dispatcher.message_handler(commands=['/tomorrow'])
+async def send_tomorrow(message:types.Message):
+    for lesson in resurn_homeworks(int(strftime('%d')) + 1):
+        await message.answer(lesson)
 
 
 executor.start_polling(dispatcher, skip_updates=True)
--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import requests
-from urllib3 import disable_warnings
+import re
 import json
+from urllib3 import disable_warnings
 from bs4 import BeautifulSoup
 from rich import print as rint
 
@@ -22,8 +23,32 @@ FormData = {
 }
 
 
+def search_time(string):
+    regexp_time_24 = re.search(r'[0-9]{2}:[0-9]{2}', string)
+    regexp_time_12 = re.search(r'[0-9]{2}:[0-9]{2} (AM|PM)', string) # search "07:00 AM" at string
+    if regexp_time_12:
+        regexp_split = regexp_time_12.group().split() # ['07:00', 'AM']
+        regexp_time = regexp_split[0].split(':') # ['07', '00']
+        if regexp_split[1] == 'PM' and regexp_time[0] != '12':
+            lesson_time = ':'.join((str(int(regexp_time[0]) + 12), regexp_time[1])) # convert 12 time to 24 time
+        else:
+            lesson_time = regexp_split[0]
+    elif regexp_time_24:
+        lesson_time = regexp_time.group()
+    else: 
+        lesson_time = 'урок без времени'
+    return lesson_time
+
+
+def search_link(string):
+    regexp_link = re.search(r'https?://[\S][^>]+', string)
+    link = regexp_link.group() if regexp_link else 'ссылок нет'
+    return link
+
+
 def update_site(filename):
-    assert filename.endswith('.html'), "type is not html"
+    if not filename.endswith('.html'):
+        assert ValueError('filetype is not html')
 
     with open(filename, "w", encoding="UTF-8") as file:
         file.write(str(pupil_bs))
@@ -32,37 +57,39 @@ def update_site(filename):
 def write_table(filename, days, lessons, homeworks):
     with open(filename, "w", encoding="UTF-8") as file:
         x = 0
-        for lesson, homework in zip(lessons, homeworks):
-            if lesson.startswith('1.'):
+        for i, j in zip(lessons, homeworks):
+            if "1." in i and "11." not in i:
                 file.write(f"\n{days[x]}\n")
                 x += 1
-            if len(lesson) > 3:
-                file.write(f"{lesson} {homework}\n")
+            file.write(f"{i} {j}\n") if len(i) > 3 else None
 
 
-def create_table(days: list, lessons: list, homeworks: list) -> dict:
+def create_table(days: list, lessons: list, homeworks: list):
     result = dict()
     score = -1
 
-    for line in zip(lessons, homeworks):
-        if line[0].startswith('1.'):
+    for string in zip(lessons, homeworks):
+        if string[0].startswith('1.'):
             score += 1
         try:
-            result[days[score].split()[0]].append((int(line[0][0]),
-                                                   line[0].split(maxsplit=1)[-1] if len(line[0]) > 3 else '',
-                                                   line[1]))
+            result[days[score].split()[0]].append((int(string[0][0]),
+                                                   string[0].split(maxsplit=1)[-1] if len(string[0]) > 3 else '', # lesson
+                                                   string[1],
+                                                   search_time(string[1]),
+                                                   search_link(string[1])))
         except KeyError:
             result[days[score].split()[0]] = [int(days[score][-2:]),
-                                              (int(line[0][0]),
-                                               line[0].split(maxsplit=1)[-1] if len(line[0]) > 3 else '', line[1])]
+                                              (1, string[0].split(maxsplit=1)[-1] if len(string[0]) > 3 else '', 
+                                              string[1],
+                                              search_time(string[1]),
+                                              search_link(string[1]))]
 
     return result
 
-
-server_answer = 0
-while server_answer != 200:
+responce = 0
+while responce != 200:
     school5p = session.post("https://school-5p.e-schools.info/login_", data=FormData)
-    server_answer = school5p.status_code
+    responce = school5p.status_code
 
 shodennik = session.get('https://school-5p.e-schools.info/pupil/1056949/dnevnik/quarter/28553')
 print('school5p connected')
@@ -70,18 +97,24 @@ print('school5p connected')
 pupil_bs = BeautifulSoup(shodennik.content, "html.parser")
 print('site parsed')
 
-week_days = [day.text for day in pupil_bs.select('th.lesson')]
-days_lessons = [" ".join(str(lesson_line.text).split()) for lesson_line in pupil_bs.select("td.lesson > span")]
+week_days = [i.text for i in pupil_bs.select('th.lesson')]
+days_lessons = [" ".join(str(i.text).split()) for i in pupil_bs.select("td.lesson > span")]
 lessons_homeworks = [" ".join(str(i.text).split()) for i in pupil_bs.select("td.ht")]
 print('school data initialised')
 
-if __name__ == '__main__':
-    update_site('parsed.html')
-    print('parsed.html is rewrited')
-    write_table('homeworks.txt', days=week_days, lessons=days_lessons, homeworks=lessons_homeworks)
-    print('homeworks.txt rewrited')
-    all_of_week = create_table(days=week_days, lessons=days_lessons, homeworks=lessons_homeworks)
-    with open('homeworks.json', 'w', encoding='UTF-8') as json_file:
-        json.dump(all_of_week, json_file, ensure_ascii=False, indent=4)
-    print('homeworks.json rewrited')
-    rint('Программа завершена [green]успешно[/green]. Расписание в файле [cyan]homeworks.txt[/cyan]')
+update_site('parsed.html')
+write_table('homeworks.txt', days=week_days, lessons=days_lessons, homeworks=lessons_homeworks)
+all_of_week = create_table(days=week_days, lessons=days_lessons, homeworks=lessons_homeworks)
+with open('homeworks.json', 'w', encoding='UTF-8') as json_file:
+    json.dump(all_of_week, json_file, ensure_ascii=False, indent=4)
+print('homeworks.json rewrited')
+
+# for days, lessons in list(all_of_week.items()):
+#     for i in range(1, len(lessons)): 
+#         if datetime.now().day == lessons[0]:
+#             # lessons[i] is (num, lesson, homeworks)
+#             pass
+#             #print(template.format(lesson=lessons[i][1], link=link, time=lesson_time))
+
+
+rint('Программа завершена [green]успешно[/green]. Расписание в файле [cyan]homeworks.txt[/cyan]')
