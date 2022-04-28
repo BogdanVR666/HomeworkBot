f508df4094a8f4efccf7086ebbab25fac4be5ebb
BogdanVR666 <63509709+BogdanVR666@users.noreply.github.com>
2022-04-28 22:53:37 +0300

    some updates

--- main.py
+++ main.py
@@ -16,17 +16,19 @@ template = '''
 Ссылка: {}
 '''
 
+bot = Bot(token='5102803513:AAEJRgR_XxoaCQYG81MwTX9zLPxMiGR9vYs')
+dispatcher = Dispatcher(bot)
+
+
 async def timer(time):
     while datetime.now() > time:
         asyncio.sleep(30)
 
-bot = Bot(token='5102803513:AAEJRgR_XxoaCQYG81MwTX9zLPxMiGR9vYs')
-dispatcher = Dispatcher(bot)
 
-def return_homeworks(day):
+def return_homeworks(date):
     result = list()
     for day in homeworks.values():
-        if day[0] == day:
+        if day[0] == date:
             for lesson in day:
                 if isinstance(lesson, list):
                     result.append(template.format(*lesson))
@@ -35,20 +37,38 @@ def return_homeworks(day):
 
 @dispatcher.message_handler(commands=['yesteday'])
 async def send_yestesay(message: types.Message):
-    for lesson in return_homeworks(int(strftime('%d')) - 1):
-        await message.answer(lesson)
+    if lessons:=return_homeworks(int(strftime('%d')) - 1):
+        for lesson in lessons:
+            await message.answer(lesson)
+    else:
+        await message.answer('Даже я не знаю, какие вчера были уроки')
 
 
 @dispatcher.message_handler(commands=['today'])
 async def send_today(message: types.Message):
-    for lesson in return_homeworks(int(strftime('%d'))):
-        await message.answer(lesson)
+    if lessons:=return_homeworks(int(strftime('%d'))):
+        for lesson in lessons:
+            await message.answer(lesson)
+    else: 
+        await message.answer('Даже я не знаю, какие сегодня уроки')
 
 
-@dispatcher.message_handler(commands=['/tomorrow'])
+@dispatcher.message_handler(commands=['now'])
+async def send_now(message: types.Message):
+    for day in lessons.values():
+        if day[0] == int(strftime('%d')):
+            for index, lesson in enumerate(day):
+                if isinstance(lesson, list):
+                    
+
+
+@dispatcher.message_handler(commands=['tomorrow'])
 async def send_tomorrow(message:types.Message):
-    for lesson in resurn_homeworks(int(strftime('%d')) + 1):
-        await message.answer(lesson)
+    if lessons:=return_homeworks(int(strftime('%d')) + 1):
+        for lesson in lessons:
+            await message.answer(lesson)
+    else:
+        await message.answer('Даже я не знаю, какие завтра уроки')
 
 
 executor.start_polling(dispatcher, skip_updates=True)
