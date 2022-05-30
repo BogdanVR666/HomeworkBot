fc2b5905964defb8d9dc3c081d5548e9a4b4878b
BogdanVR <bogdanvr666@gmail.com>
2022-05-30 20:03:42 +0300

    remowed fatal files

--- keyboard.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-yesterday_button = KeyboardButton('/yesterday')
-today_button = KeyboardButton('/today')
-tomorrow_button = KeyboardButton('/tomorrow')
-now_button = KeyboardButton("/now")
-
-menu = ReplyKeyboardMarkup(resize_keyboard=True).add(yesterday_button, today_button, tomorrow_button, now_button)
